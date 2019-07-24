924adcbaf346015dfba9fc5d1f26d4ae63ea9bd0
Seung-Tak Noh <noh@ui.is.s.u-tokyo.ac.jp>
2019-07-24 21:41:51 +0900

    # example102: add usages of cvgl
    - generate color/depth image from GL rendering (FBO & ConvertCVGL)
    - point cloud conversion from depth image (ConvertCVGL)
    - point cloud visualization (DrawGL3D)

--- examples/example102/AppGL.cpp
+++ examples/example102/AppGL.cpp
@@ -1,11 +1,10 @@
 #include "AppGL.h"
 #include "cvgl/DrawGL3D.h"
-
-#include <opencv2/opencv.hpp>
+#include "cvgl/ConvertCVGL.h"
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// mandatory
+// mandatory member function
 ///////////////////////////////////////////////////////////////////////////////
 void AppGL::Draw(const int width, const int height)
 {
@@ -52,7 +51,7 @@ void AppGL::drawView3D(glm::mat4 proj, glm::mat4 view)
 	glm::mat4 cam_proj = rendercam->GetProjMatrix();
 	glm::mat4 cam_pose = rendercam->GetPoseMatrix();
 
-	// draw 3D axes
+	// draw 3D axes (with lighting)
 	glEnable(GL_LIGHTING);
 	cvgl::setLight(GL_LIGHT0, glm::vec4(-1.0f, -1.0f, -1.0f, 0.0f));
 	cvgl::drawAxes(1.0f);
@@ -61,7 +60,6 @@ void AppGL::drawView3D(glm::mat4 proj, glm::mat4 view)
 		cvgl::drawAxes(1.0f);
 		glPopMatrix();
 	}
-
 	glDisable(GL_LIGHTING);
 
 	// draw 3D grid
@@ -69,20 +67,26 @@ void AppGL::drawView3D(glm::mat4 proj, glm::mat4 view)
 	glColor3f(1.0f, 1.0f, 1.0f);
 	cvgl::drawGridXZ(20.0, 20);
 
-	// draw camera frustum
-	glLineWidth(2.0f);
 	glColor3f(1.0f, 1.0f, 0.0f);
 	{
 		glPushMatrix();
 		glMultMatrixf(glm::value_ptr(cam_pose));
+
+		// camera frustum
+		glLineWidth(2.0f);
 		cvgl::drawCameraFrustum(cam_proj);
+
+		// point cloud
+		glPointSize(1.0f);
+		cvgl::drawPointCloud(cloud);
+
 		glPopMatrix();
 	}
 }
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// 
+// override member functions
 ///////////////////////////////////////////////////////////////////////////////
 bool AppGL::Init()
 {
@@ -93,6 +97,9 @@ bool AppGL::Init()
 
 	resetGlobalView();
 	resetCameraView();
+
+	// instantiate frame buffer object
+	offscreenFBO = new cvgl::FBO();
 
 	////////////////////////////////////////////////////////////
 	// UI: AntTweakBar
@@ -110,7 +117,7 @@ bool AppGL::Init()
 	TwAddVarRW(bar, "Global-posZ", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.z, "group='Global' label='posZ' step=0.01");
 #endif
 
-	// local camera viewer
+	// view matrix related properties (local camera)
 #if 1
 	TwAddButton(bar, "Local-init", [](void *client) {
 		AppGL* _this = (AppGL*)client; _this->resetCameraView();
@@ -119,7 +126,17 @@ bool AppGL::Init()
 	TwAddVarRW(bar, "Local-posX", TwType::TW_TYPE_FLOAT , &rendercam->position.x, "group='Local' label='posX' step=0.01");
 	TwAddVarRW(bar, "Local-posY", TwType::TW_TYPE_FLOAT , &rendercam->position.y, "group='Local' label='posY' step=0.01");
 	TwAddVarRW(bar, "Local-posZ", TwType::TW_TYPE_FLOAT , &rendercam->position.z, "group='Local' label='posZ' step=0.01");
+#endif
 
+	// point cloud from rendered depth image
+	TwAddButton(bar, "CreatePointCloud",
+		[](void* client) {
+			AppGL* _this = (AppGL*)client;
+			_this->CreatePointCloud();
+		}, this, "key=SPACE");
+
+	// projection matrix related properties
+#if 1
 	TwAddVarRW(bar, "Camera-ortho"   , TwType::TW_TYPE_BOOLCPP, &rendercam->is_ortho, "group='Camera' label='ortho'");
 	TwAddVarRW(bar, "Camera-infinity", TwType::TW_TYPE_BOOLCPP, &rendercam->is_inf  , "group='Camera' label='infinity'");
 	TwAddVarRW(bar, "Camera-z_near"  , TwType::TW_TYPE_FLOAT  , &rendercam->z_near  , "group='Camera' label='z_near' step=0.01");
@@ -140,13 +157,66 @@ bool AppGL::Init()
 	TwAddVarRW(bar, "Camera-R", TwType::TW_TYPE_FLOAT, &rendercam->R, "group='Camera' label='Right'  step=0.01");
 	TwAddVarRW(bar, "Camera-B", TwType::TW_TYPE_FLOAT, &rendercam->B, "group='Camera' label='Bottom' step=0.01");
 	TwAddVarRW(bar, "Camera-T", TwType::TW_TYPE_FLOAT, &rendercam->T, "group='Camera' label='Top'    step=0.01");
+
+	TwDefine("Bar/Camera opened=false"); // close group as default
 #endif
 
 	return true;
 }
 void AppGL::End()
 {
-	if (rendercam) delete rendercam;
+	if (rendercam   ) delete rendercam;
+	if (offscreenFBO) delete offscreenFBO;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// user-defined function
+///////////////////////////////////////////////////////////////////////////////
+void AppGL::CreatePointCloud()
+{
+	// clear point cloud in advance
+	cloud.clear();
+
+	////////////////////////////////////////
+	// offscreen rendering to generate data
+	////////////////////////////////////////
+	int w = width / 2;
+	int h = w;
+
+	offscreenFBO->Resize(w, h);
+	offscreenFBO->Enable();
+
+	// draw 3D scene
+	glm::mat4 proj = rendercam->GetProjMatrix();
+	glm::mat4 view = rendercam->GetViewMatrix();
+
+	glClearColor(BgColor[1].r, BgColor[1].g, BgColor[1].b, BgColor[1].a);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	drawView3D(proj, view);
+	glDisable(GL_DEPTH_TEST);
+
+	// get image as cv::Mat from GL buffer
+	cv::Mat color = cvgl::GetRenderedColorImage8U(w, h);
+	cv::Mat depth = cvgl::GetRenderedDepthImage32F(w, h);
+
+	offscreenFBO->Disable();
+
+	////////////////////////////////////////
+	// post-process
+	////////////////////////////////////////
+
+	// image sanity check
+#if 1
+	cv::imshow("color", color);
+	cv::imshow("depth", depth);
+#endif
+
+	// convert depth image to point cloud
+	cloud = cvgl::ConvertDepthImage2PointCloud(proj, depth);
+
+	return;
 }
 
 
--- examples/example102/AppGL.h
+++ examples/example102/AppGL.h
@@ -4,27 +4,34 @@
 #include "cvgl/AppGLBase.h"
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
+#include <opencv2/highgui.hpp>
 
 #include "cvgl/GLCamera.h"
+#include "cvgl/FBO.h"
+
 
 class AppGL : public AppGLBase
 {
 public:
 	AppGL(const int width, const int height) : AppGLBase(width, height) {};
 
+	// mandatory member function
 	void Draw(const int width, const int height);
 
 	// override member functions
 	bool Init();
 	void End();
 
-private:
+	// user-defined function
+	void CreatePointCloud();
 
+private:
 	void drawView3D(glm::mat4 proj, glm::mat4 view);
 
 	// member objects & variables
 	glm::fvec4 BgColor[2] = { glm::fvec4(0.5, 0.5, 0.5, 1.0), glm::fvec4(0.2, 0.3, 0.5, 1.0) };
 
+	// global viewer
 	glm::quat GlobalViewRotation;
 	glm::vec3 GlobalViewPosition;
 	void resetGlobalView() {
@@ -32,12 +39,17 @@ private:
 		GlobalViewRotation = glm::quat(glm::radians(glm::vec3(45.0f, -37.5f, -30.0f)));
 	}
 
+	// camera object
 	cvgl::GLCamera *rendercam = nullptr;
 	void resetCameraView() {
 		// oblique view as default
 		rendercam->position = glm::vec3(0.0f, 10.0f, 10.0f);
 		rendercam->rotation = glm::quat(glm::radians(glm::vec3(-45.0f, 0.0f, 0.0f)));
 	}
+
+	// offscreen renderer
+	cvgl::FBO *offscreenFBO = nullptr;
+	std::vector<glm::vec3> cloud;
 };
 
 #endif
