a929315415d9a4603f955dbff954a01f52e11e22
Seung-Tak Noh <noh@ui.is.s.u-tokyo.ac.jp>
2019-07-30 02:43:05 +0900

    # update example201
    - local  viewer: sync with selected camera (image)
    - global viewer: depth/color camera frustums and 3D point cloud

--- examples/example201/Example201.cpp
+++ examples/example201/Example201.cpp
@@ -1,5 +1,6 @@
 #include "Example201.h"
 #include <cvgl/DrawGL2D.h>
+#include <cvgl/DrawGL3D.h>
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -10,18 +11,23 @@ void Example201::Draw(const int width, const int height)
 	// left / right
 	int w = width / 2;
 
-	cv::Size color_size = camera->GetColorImageSize();
-	cv::Size depth_size = camera->GetDepthImageSize();
-	float color_ratio = (float)color_size.width / (float)color_size.height;
-	float depth_ratio = (float)depth_size.width / (float)depth_size.height;
+	cv::Mat img;
+	double scale = 255.0 / 1200.0;
 
-	cv::Mat img[2] = { camera->GetColorImage(), camera->GetIrImage() };
+	cv::Size imgSize = cv::Size(w, height);
+	switch (imgType) {
+	case COLOR: img = camera->GetColorImage(); imgSize = camera->GetColorImage().size(); scale = 1.0; break;
+	case DEPTH: img = camera->GetDepthImage(); imgSize = camera->GetDepthImage().size(); break;
+	case IR   : img = camera->GetIrImage();    imgSize = camera->GetIrImage().size();    break;
+	default: break;
+	}
+	float aspect_inv = (float)imgSize.height / (float)imgSize.width;
+	int h = (int)(aspect_inv * (float)w);
 
-	// viewport settings
-	int img_height = glm::min(color_size.height , depth_size.height);
+	// viewport settings ( 0:left / 1:right )
 	cv::Rect viewport[2] = {
-		cv::Rect(0, 0, w, w / color_ratio), // left : global viewer
-		cv::Rect(w, 0, w, w / depth_ratio), // right: selected camera view
+		cv::Rect(0, 0, w, height),
+		cv::Rect(w, 0, w, h)
 	};
 
 	glEnable(GL_SCISSOR_TEST); // to cut left/right viewport
@@ -33,9 +39,50 @@ void Example201::Draw(const int width, const int height)
 		glClearColor(BgColor[id].r, BgColor[id].g, BgColor[id].b, BgColor[id].a);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		cvgl::drawImage(img[id], viewport[id]);
+		if (0 == id) {
+			// left side: global viewer
+			glm::mat4 proj = glm::infinitePerspective(glm::radians(53.1301f), (float)viewport[id].width / (float)viewport[id].height, 0.1f);
+			glm::mat4 view = glm::translate(glm::mat4(1.0f), -GlobalViewPosition);
+			view = view * glm::toMat4(GlobalViewRotation);
+
+			glEnable(GL_DEPTH_TEST);
+			drawView3D(proj, view);
+			glDisable(GL_DEPTH_TEST);
+		}
+		else {
+			// right side: local viewer
+			glDisable(GL_DEPTH_TEST);
+			if (show_raw_image) cvgl::drawImage(img, viewport[id], scale);
+			glEnable(GL_DEPTH_TEST);
+
+			// 3D point cloud in local view
+			glm::mat4 proj = (COLOR == imgType) ? colorCamGL->GetProjMatrix() : depthCamGL->GetProjMatrix();
+			glm::mat4 view = (COLOR == imgType) ? colorCamGL->GetViewMatrix() : depthCamGL->GetViewMatrix();
+			drawView3D(proj, view);
+			glDisable(GL_DEPTH_TEST);
+		}
 	}
 	glDisable(GL_SCISSOR_TEST);
+}
+void Example201::drawView3D(glm::mat4 proj, glm::mat4 view)
+{
+	glMatrixMode(GL_PROJECTION); glLoadMatrixf(glm::value_ptr(proj));
+	glMatrixMode(GL_MODELVIEW);  glLoadMatrixf(glm::value_ptr(view));
+
+	// color camera
+	glColor3f(1.0f, 0.0f, 0.0f);
+	colorCamGL->SetCameraCoord([](){}, true);
+
+	// depth camera
+	glColor3f(1.0f, 1.0f, 0.0f);
+	depthCamGL->SetCameraCoord([&](){
+			if (show_pointcloud) {
+				std::vector<glm::vec3>   points;
+				std::vector<glm::u8vec3> colors;
+				camera->GetPointCloud(points, colors);
+				cvgl::drawPointCloud(points, colors);
+			}
+		}, true);
 }
 
 
@@ -48,12 +95,54 @@ bool Example201::Init()
 	camera = new cvgl::RSCamera();
 	camera->InitLive();
 
+	// color camera
+	colorCamGL = new cvgl::GLCamera();
+	{
+		std::vector<float> camParams4x1; glm::mat4 pose;
+		camera->GetCalibData(1, camParams4x1, pose);
+
+		colorCamGL->SetCameraMatrixCV(camParams4x1, 0.01f);
+		colorCamGL->SetPoseMatrixGL(pose);
+	}
+
+	// depth camera: 15[cm] - 1.2[m]
+	depthCamGL = new cvgl::GLCamera();
+	{
+		std::vector<float> camParams4x1; glm::mat4 pose;
+		camera->GetCalibData(0, camParams4x1, pose);
+
+		depthCamGL->SetCameraMatrixCV(camParams4x1, 0.15f, 1.20f);
+		depthCamGL->SetPoseMatrixGL(pose);
+	}
+
+	// initialize global viewer
+	resetGlobalView();
+
 	////////////////////////////////////////////////////////////
 	// UI: AntTweakBar
 	////////////////////////////////////////////////////////////
-	TwDefine("Bar size='620 100'");
 
-	// [TODO] change image type
+	// global viewer (3D point cloud)
+#if 1
+	TwAddButton(bar, "Global-init", [](void *client) {
+		Example201* _this = (Example201*)client; _this->resetGlobalView();
+	}, this, "group='Global' label='init' ");
+	TwAddVarRW(bar, "Global-rot", TwType::TW_TYPE_QUAT4F, &GlobalViewRotation, "group='Global' label='rot'  open");
+	TwAddVarRW(bar, "Global-posX", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.x, "group='Global' label='posX' step=0.01");
+	TwAddVarRW(bar, "Global-posY", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.y, "group='Global' label='posY' step=0.01");
+	TwAddVarRW(bar, "Global-posZ", TwType::TW_TYPE_FLOAT, &GlobalViewPosition.z, "group='Global' label='posZ' step=0.01");
+#endif
+
+	// local viewer (2D image)
+	std::vector<TwEnumVal> imageTypeEV = { { RawImage::INVALID, "<EMPTY>" },
+		{ RawImage::COLOR, "Color" },
+		{ RawImage::DEPTH, "Depth" },
+		{ RawImage::IR   , "Infrared" } };
+	TwType twImageType = TwDefineEnum("Raw image", &imageTypeEV[0], (unsigned int)imageTypeEV.size());
+	TwAddVarRW(bar, "Raw image", twImageType, &imgType, NULL);
+
+	TwAddVarRW(bar, "Show raw image" , TW_TYPE_BOOLCPP, &show_raw_image , NULL);
+	TwAddVarRW(bar, "Show pointcloud", TW_TYPE_BOOLCPP, &show_pointcloud, NULL);
 
 	return true;
 }
@@ -64,6 +153,9 @@ void Example201::End()
 		camera->End();
 		delete camera;
 	}
+
+	if (colorCamGL) delete colorCamGL;
+	if (depthCamGL) delete depthCamGL;
 }
 
 bool Example201::Update()
--- examples/example201/Example201.h
+++ examples/example201/Example201.h
@@ -4,8 +4,10 @@
 #include <cvgl/AppGLBase.h>
 
 #include <glm/glm.hpp>
+#include <glm/ext.hpp>
 #include <opencv2/highgui.hpp>
 
+#include <cvgl/GLCamera.h>
 #include <cvgl/RSCamera.h>
 
 class Example201 : public AppGLBase
@@ -23,11 +25,28 @@ public:
 	bool Update();
 
 private:
+	void drawView3D(glm::mat4 proj, glm::mat4 view);
 
 	// member objects & variables
 	glm::fvec4 BgColor[2] = { glm::fvec4(0.5, 0.5, 0.5, 1.0), glm::fvec4(0.2, 0.3, 0.5, 1.0) };
 
+	// global viewer
+	glm::quat GlobalViewRotation;
+	glm::vec3 GlobalViewPosition;
+	void resetGlobalView() {
+		GlobalViewPosition = glm::vec3(0.75f, 0.50f, 1.25f);
+		GlobalViewRotation = glm::quat(glm::radians(glm::vec3(45.0f, -37.5f, -30.0f)));
+	}
+
+	// local viewer
+	enum RawImage { INVALID = -1, COLOR, DEPTH, IR } imgType = RawImage::COLOR;
+	bool show_raw_image  = true;
+	bool show_pointcloud = true;
+
+	// RGB-D camera & camera objects
 	cvgl::RSCamera *camera = nullptr;
+	cvgl::GLCamera *colorCamGL = nullptr;
+	cvgl::GLCamera *depthCamGL = nullptr;
 };
 
 #endif
